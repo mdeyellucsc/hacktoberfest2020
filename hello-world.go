6291daa1f9421f8e582497ced95b3fc939a2fdbf
Matthew Deyell <mdeyell@Matthews-MacBook-Pro.local>
2020-10-21 20:24:05 -0700

    time example

--- hello-world.go
+++ hello-world.go
@@ -3,6 +3,7 @@ package main
 import (
     "crypto/sha1"
     "fmt"
+    "time"
 )
 
 func main() {
@@ -35,4 +36,30 @@ func main() {
     bs := h.Sum(nil)
     fmt.Println(s)
     fmt.Printf("%x\n", bs)
+
+    p := fmt.Println
+
+    t := time.Now()
+    p(t.Format(time.RFC3339))
+
+    t1, e := time.Parse(
+        time.RFC3339,
+        "2012-11-01T22:08:41+00:00")
+    p(t1)
+
+    p(t.Format("3:04PM"))
+    p(t.Format("Mon Jan _2 15:04:05 2006"))
+    p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+    form := "3 04 PM"
+    t2, e := time.Parse(form, "8 41 PM")
+    p(t2)
+
+    fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+        t.Year(), t.Month(), t.Day(),
+        t.Hour(), t.Minute(), t.Second())
+
+    ansic := "Mon Jan _2 15:04:05 2006"
+    _, e = time.Parse(ansic, "8:41PM")
+    p(e)
+
 }
